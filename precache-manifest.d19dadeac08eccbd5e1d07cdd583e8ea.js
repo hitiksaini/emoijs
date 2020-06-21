976e668a1a32ed682f51799049ec4b835fcf2a51
hitiksaini <hitik9045saini@gmail.com>
2020-06-21 12:40:46 +0530

    Updates

---
+++ precache-manifest.d19dadeac08eccbd5e1d07cdd583e8ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7514f8e323176ca18d0ae3b9d98e6779",
+    "url": "/emojis/index.html"
+  },
+  {
+    "revision": "78148f6365a3ab6a92d4",
+    "url": "/emojis/static/js/2.c250aed4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emojis/static/js/2.c250aed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0937adab3cf0b3c76b45",
+    "url": "/emojis/static/js/main.657a1290.chunk.js"
+  },
+  {
+    "revision": "d9097ab21a0ed1716533",
+    "url": "/emojis/static/js/runtime-main.a02f6d6d.js"
+  }
+]);
